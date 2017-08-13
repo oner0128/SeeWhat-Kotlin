7c9e548e2fe4e88a3f32a85c18d7b21f83ca926a
oner0128 <oner0128@gmail.com>
2017-08-13 17:05:52 +0800

    changed meiziFragment imageView to photoView

--- app/src/main/java/com/onerous/kotlin/seewhat/meizi/MeiziPictureActivity.kt
+++ app/src/main/java/com/onerous/kotlin/seewhat/meizi/MeiziPictureActivity.kt
@@ -1,9 +1,12 @@
 package com.onerous.kotlin.seewhat.meizi
 
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.animation.GlideAnimation
+import com.bumptech.glide.request.target.SimpleTarget
 import com.onerous.kotlin.seewhat.R
 import kotlinx.android.synthetic.main.activity_meizi_picture.*
 
@@ -16,12 +19,11 @@ class MeiziPictureActivity : AppCompatActivity() {
         val url=intent.getStringExtra("meiziUrl")
         Glide.with(this)
                 .load(url)
-                .centerCrop()
                 .into(iv_meizi)
-    }
-
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        finish()
-        return super.onTouchEvent(event)
+        //点击一次退出
+        iv_meizi.setOnClickListener({view->
+            finish()
+            true
+        })
     }
 }
